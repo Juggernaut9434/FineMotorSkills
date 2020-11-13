9bcd48949f921bf4ccbb22dd28a1c5d3617dc87c
Mike <juggernaut9434@gmail.com>
2020-11-13 15:34:07 -0700

    Update StartToFinish.js
    only the start to finish logic

--- StartToFinish.js
+++ StartToFinish.js
@@ -4,74 +4,190 @@
 // from one side of the screen to the other using
 // a mouse or finger
 
-var startX, startY, finishX, finishY;
+let song;
+let startX, startY, finishX, finishY; // drawing the line variables
+let o1, o2; // object variables for shapes, TODO move to 
 
+function preload() {
+  song = loadSound("asset/Cool-Adventure-Intro.mp3");
+  // https://p5js.org/reference/#/p5.SoundFile 
+  // song From https://soundimage.org/action/
+}
+
+/***************************************************
+ * SETUP is called once at the beginning
+ ***************************************************/
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(600, 400); // always have the same canvas of 600x400
+  //song.play();    // play the song
   background("pink");
-  
-  drawRects("red");
-}
-
-function draw() {
-}
-
-function drawRects(color="red") {
-  // middle box
-  fill(255,255,255,0);
-  rect(150, 175, 300, 50);
-  //two red boxes
-  fill(color);
-  rect(50, 400/2 - 25, 100, 50);
-  rect(450, 400/2 - 25, 100, 50);
-  //add text to the red boxes
-  fill("black");
-  textSize(30);
-  text("Start", 50+15, height/2+10);
-  text("Finish", 450+10, height/2+10);
-}
-
-function mousePressed() { 
-  startX = mouseX;
-  startY = mouseY;
-}
-function mouseDragged() {
-  setup();
-  finishX = mouseX;
-  finishY = mouseY;
-  strokeWeight(10);
+  // randomly set the locations of the two boxes
+  let minX = 20,
+    maxX = 450,
+    minY = 20,
+    maxY = 350;
+  o1 = new Obstacle(random(minX, maxX / 2 - 50), random(minY, maxY));
+  o2 = new Obstacle(random(maxX / 2 + 50, maxX), random(minY, maxY));
+  // draw dashed line
   stroke(0);
-  line(startX, startY, finishX, finishY);
-  noStroke();
+  fill(0);
+  strokeWeight(1);
+  linedash(o1.x + o1.width / 2, o1.y + o1.height / 2,
+    o2.x + o2.width / 2, o2.y + o2.height / 2, 10);
 }
+
+/***************************************************
+ * DRAW is called continually, looping onto the canvas
+ ***************************************************/
+function draw() {
+  // draw a dashed line
+  fill(0);
+  textSize(30);
+  linedash(o1.x + o1.width / 2, o1.y + o1.height / 2,
+    o2.x + o2.width / 2, o2.y + o2.height / 2, 10);
+}
+
+/***************************************************
+ * User ACTIONS called when action is activated, pressing mouse etc.
+ ***************************************************/
+function mousePressed() {
+  // log the initial point
+  start = new Point(mouseX, mouseY);
+}
+
+function mouseDragged() {
+  // redraw the beginning
+  // to prevent multiple lines drawn at once
+  background("pink");
+  o1.show();
+  o2.show();
+  // log the end point
+  finish = new Point(mouseX, mouseY);
+  //draw the line
+  stroke(0);
+  line(start.x, start.y, finish.x, finish.y);
+  // draw the dashed line
+  fill(0);
+  strokeWeight(1);
+  linedash(o1.x + o1.width / 2, o1.y + o1.height / 2,
+    o2.x + o2.width / 2, o2.y + o2.height / 2, 10);
+  noStroke();
+  strokeWeight(10);
+  fill(0);
+}
+
 function mouseReleased() {
-  if(startY < 180 || startY > 230
-     || finishY < 180 || finishY > 230)
-    {
-      setup();
-      strokeWeight(300);
-      text("X", 250, 80);
-    }
-  else{
-    strokeWeight(300);
+  // if line is inside boxes
+  if ((inside(o1, start) && inside(o2, finish)) ||
+    (inside(o1, finish) && inside(o2, start))) {
+    // draw the smile face
+    fill(0); // color of smiley face
+    textSize(30);
     text(":)", 250, 80);
     noStroke();
-    drawRects("green");
+    // change boxes color to green
+    o1.clr = "green";
+    o2.clr = "green";
+    // wait a few seconds and redo game
+    setTimeout(() => {
+      setup();
+    }, 1250);
+    return;
+  } else {
+
+    // redraw the beginning
+    // to prevent multiple lines drawn at once
+    background("pink");
+    o1.show();
+    o2.show();
+    // draw the line
+    stroke(0);
+    fill(0);
+    strokeWeight(1);
+    linedash(o1.x + o1.width / 2, o1.y + o1.height / 2,
+      o2.x + o2.width / 2, o2.y + o2.height / 2, 10);
+    noStroke();
+    strokeWeight(10);
+    // show x;
+    fill(255, 0, 0)
+    textSize(30);
+    text("X", 250, 80);
+  }
+}
+/*************Helper**Functions**********************/
+
+/*
+ * return true if point is inside rectangle
+ * ox is an Obstacle, point is a Point
+ */
+function inside(ox, point) {
+  // if the object's x coordinate < mouse or the point x coordiante
+  // and point is less than x coord + width,
+  return (ox.x < point.x && ox.x + ox.width > point.x) &&
+    (ox.y < point.y && ox.y + ox.height > point.y);
+}
+
+function linedash(x1, y1, x2, y2, delta = 1) {
+  // delta is both the length of a dash, the distance
+  // between 2 dots/dashes, and the diameter of a round
+  fill(255);
+  let distance = dist(x1, y1, x2, y2);
+  let dashNumber = distance / delta;
+  let xDelta = (x2 - x1) / dashNumber;
+  let yDelta = (y2 - y1) / dashNumber;
+
+  for (let i = 0; i < dashNumber; i += 2) {
+    let xi1 = i * xDelta + x1;
+    let yi1 = i * yDelta + y1;
+    let xi2 = (i + 1) * xDelta + x1;
+    let yi2 = (i + 1) * yDelta + y1;
+    line(xi1, yi1, xi2, yi2);
   }
 }
 
-function drawArrow(base, vec, myColor) {
-  push();
-  stroke(myColor);
-  strokeWeight(3);
-  fill(myColor);
-  translate(base.x, base.y);
-  line(0, 0, vec.x, vec.y);
-  rotate(vec.heading());
-  let arrowSize = 7;
-  translate(vec.mag() - arrowSize, 0);
-  triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
-  pop();
+// display error message
+function errorMsg(name, debug = 0) {
+  console.log("Something Went Wrong:", name, "\tgame", game);
+  if (debug != 0) {
+    background(255);
+    fill(255, 0, 0);
+    textSize(50);
+    textAlign(CENTER, CENTER);
+    text("Something Went Wrong", name, 300, 200);
+  }
+}
+
+/*************Classes*****************************/
+class Obstacle {
+  constructor(x, y, w = 100, h = 50, cr = "blue") {
+    this.x = x;
+    this.y = y;
+    this.c = color(cr);
+    this.width = w;
+    this.height = h;
+    this.show();
+  }
+  // get color
+  get clr() {
+    return this.c;
+  }
+  // change the color
+  set clr(cd) {
+    this.c = color(cd);
+    this.show();
+  }
+  // display the change.
+  show() {
+    fill(this.c);
+    rect(this.x, this.y, this.width, this.height);
+  }
+}
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 }
 
 //end
